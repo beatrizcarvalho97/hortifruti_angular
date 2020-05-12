988d64d004b2a643e97ccd7165f1177b8ebffb01
beatrizcarvalho97 <beatriz.carvalho.costa@hotmail.com>
2020-05-11 23:16:01 -0300

    Estilização da componente Home

--- hortifruti-spa/src/app/app-routing.module.ts
+++ hortifruti-spa/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LojaComponent } from './loja/loja.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'loja', component: LojaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
